f16d275ed748dca757c946e23dbe1e783fe53d20
mantagen <mathsgregory@gmail.com>
2015-10-27 14:10:47 +0000

    post requests

--- handlers.js
+++ handlers.js
@@ -1,16 +1,65 @@
 var handlers = module.exports = {};
+var qs = require('querystring');
 var fs = require('fs');
+var https = require('https');
 
-handlers.home = function (req, res) {
+handlers.home = function(req, res) {
   var index = fs.readFileSync(__dirname + '/views/index.html');
-  res.writeHead(200, {'Content-Type' : 'text/html'});
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
   res.end(index);
 };
 
 
-handlers.file = function(req, res){
-    var file = fs.readFileSync(__dirname + '/public' + req.url);
-    var ext = (req.url).split('.')[1];
-    res.writeHead(200, {'content-type' : 'text/' + ext});
-    res.end(file);
+handlers.file = function(req, res) {
+  var file = fs.readFileSync(__dirname + '/public' + req.url);
+  var ext = (req.url).split('.')[1];
+  res.writeHead(200, {
+    'content-type': 'text/' + ext
+  });
+  res.end(file);
 };
+
+
+handlers.raiseissue = function(req, res) {
+  console.log(req, req.url);
+  parseBody(req, function(body) {
+    body = qs.parse(body);
+    console.log(body);
+    postToGithub(body);
+    res.end('done');
+  });
+
+};
+
+
+function parseBody(object, callback) {
+  var body = "";
+  object.on('data', function(data) {
+    body += data;
+  });
+  object.on('end', function() {
+    callback(body);
+  });
+  object.on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+}
+
+function postToGithub(body){
+  var options = {
+      host: 'api.github.com',
+      path: '/repos/kat4/meow/issues',
+      headers: {
+       'Authorization': 'Basic ' + new Buffer(body.username + ':' + body.password).toString('base64')
+     }
+  };
+  var reqToGithub = https.request(options,function(res){
+    parseBody(res,function(){
+      console.log(body);
+    });
+  });
+  reqToGithub.write(JSON.stringify(body));
+  reqToGithub.end();
+}
